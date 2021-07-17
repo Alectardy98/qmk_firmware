e987d010f2781b92186ef9b8a88bac3d6f6f89ec
Alectardy98 <tardyalec@gmail.com>
2021-07-16 23:29:57 -0400

    fixes for the HP keymap

--- keyboards/converter/hp_46010a/keymaps/default/keymap.c
+++ keyboards/converter/hp_46010a/keymaps/default/keymap.c
@@ -154,17 +154,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,AC_FN9,  KC_F1,KC_F2,KC_F3,KC_F4,   KC_HOME,KC_PSCR,   KC_F5,KC_F6,KC_F7,KC_F8,                     AC_FN8, AC_FN7,     KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
         KC_GRAVE,     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPC,     KC_INS, KC_DEL,     KC_PAST, KC_PSLS, KC_PPLS, KC_PMNS, \
-        KC_TAB,          KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_PGUP,KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PENT, \
-        KC_CAPS, KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,  DM_PLY1, DM_PLY2,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
-        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_MPLY, KC_UP,   KC_MNXT,   KC_P1,   KC_P2,   KC_P3,   KC_TAB,  \
-        KC_LGUI, MO(1), KC_LALT,                      KC_SPACE,                            KC_RALT,MO(1), KC_LEFT, KC_DOWN, KC_RGHT,        KC_P0,       KC_PDOT           \
+        KC_TAB,          KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_PGUP,KC_PGDN,    KC_7,    KC_8,    KC_9,    KC_PENT, \
+        KC_CAPS, KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,  DM_PLY1, DM_PLY2,   KC_4,    KC_5,    KC_6,    KC_PCMM, \
+        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_MPLY, KC_UP,   KC_MNXT,   KC_1,    KC_2,    KC_3,    KC_TAB,  \
+        KC_LGUI, MO(1), KC_LALT,                      KC_SPACE,                            KC_RALT,MO(1), KC_LEFT, KC_DOWN, KC_RGHT,        KC_0,       KC_PDOT            \
     ),
     [1] = LAYOUT(
         RESET,DEBUG,  KC_F1,KC_F2,KC_F3,KC_F4,   KC_HOME,KC_PSCR,   KC_F5,KC_F6,KC_F7,KC_F8,                       AC_FN8, AC_FN7,     KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
         KC_GRAVE,     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPC,     KC_INS, KC_DEL,     KC_PAST, KC_PSLS, KC_PPLS, KC_PMNS, \
-        KC_TAB,          KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_PGUP,KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PENT, \
-        KC_CAPS, KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,  DM_REC1,DM_REC2,    KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
-        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_MPLY, AC_FN5, KC_MNXT,    KC_P1,   KC_P2,   KC_P3,   KC_TAB,  \
-        KC_LGUI, KC_TRNS, KC_LALT,                      DM_RSTP,                       KC_RALT,KC_TRNS, AC_FN2,  AC_FN6, AC_FN1,        KC_P0,       KC_PDOT             \
+        KC_TAB,          KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_PGUP,KC_PGDN,    KC_7,    KC_VOLU, KC_9,    KC_PENT, \
+        KC_CAPS, KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,  DM_REC1,DM_REC2,    KC_MPRV, KC_MPLY, KC_MNXT, KC_PCMM, \
+        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_MPLY, AC_FN5, KC_MNXT,    KC_1,    KC_VOLD, KC_3,    KC_TAB,  \
+        KC_LGUI, KC_TRNS, KC_LALT,                      DM_RSTP,                       KC_RALT,KC_TRNS, AC_FN2,  AC_FN6, AC_FN1,        KC_0,       KC_PDOT                \
     ),
 } ;
--- keyboards/converter/hp_46010a/rules.mk
+++ keyboards/converter/hp_46010a/rules.mk
@@ -34,5 +34,5 @@ LAYOUTS_HAS_RGB = no
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 CUSTOM_MATRIX = yes
-
+DYNAMIC_MACRO_ENABLE = yes
 SRC = matrix.c
